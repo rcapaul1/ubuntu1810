921b935f0483e797df189166d407dcbe4944fc6f
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-07-17 11:15:40 +0200

    Update docker-cleaner.sh

--- docker-cleaner.sh
+++ docker-cleaner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker rm -f $(docker ps -a -q) && docker volume prune -f && docker rmi -f $(docker images)
+docker rm -f $(docker ps -a -q) ; docker volume prune -f ; docker rmi -f $(docker images)
